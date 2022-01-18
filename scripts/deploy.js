3d31faed6400b84b9864151fa1b9218918b23a9b
nonseodion <nonseifebhor@gmail.com>
2022-01-18 22:04:15 +0100

    removed contract verification code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,24 +9,6 @@ async function main () {
   await crpContract.deployed();
 
   console.log(`CRP Token Deployed: ${crpContract.address}`);
-  const contractSourceCode = fs.readFileSync(
-    "/Users/nonse/Desktop/Work/CRO Predict/CRP/contracts/CRP.sol",
-    {
-      encoding: "utf-8",
-    }
-  );
-
-  try{
-    await axios.get(
-      `https://cronos-explorer.crypto.org/api?module=contract&action=verify
-       addressHash=${crpContract.address}&name=CROPredict&
-       compilerVersion=0.6.12&optimization=false&contractSourceCode=${contractSourceCode}`
-    );
-  }
-  catch(err){
-    console.log("Experienced an error when verifying contract", err.message);
-  }
-
 };
 main()
   .then(() => process.exit(0))
